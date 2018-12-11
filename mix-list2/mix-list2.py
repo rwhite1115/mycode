c3b3888696a531f85b74a23ac5d77eacd3a25e00
rwhite1115 <rwhite1115@gmail.com>
2018-12-11 14:54:37 -0500

    Append_extend

--- mix-list2/mix-list2.py
+++ mix-list2/mix-list2.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 proto = ["ssh", "http","https"]
+protoa = ["ssh", "http","https"]
 print(proto) 
+proto.append('dns') # this line will add 'dns' to the end of our list
+protoa.append('dns') # this line will add 'dns' to the end of our list
+print(proto) 
+proto2 = [ 22, 80, 443, 53 ] # a list of common ports
+proto.extend(proto2) # pass proto2 as an  agruement to the extend method -- then print results
+print (proto)
+protoa.append(proto2)# pass proto2 as an argument to the append method -- the print results
+print(protoa)
