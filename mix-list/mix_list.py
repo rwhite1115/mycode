7dbf73c640e328373288058a3cffe7b9706053dc
rwhite1115 <rwhite1115@gmail.com>
2018-12-11 13:42:29 -0500

    test_integers

--- mix-list/mix_list.py
+++ mix-list/mix_list.py
@@ -13,11 +13,12 @@ print("The first item in the list (IP): " + my_list[0] )
 
 print("The second item in the list (port): " + str(my_list[1]) )
 
-# This code will pull the last line in the list
+# This print will pull the last line in the list
 print("The last item in the list (state): " + my_list[2] )
 
-#New List
+# New List
 
 new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
+# This prints new list in one line. 
 print("When I " + new_list[5] + " into IP addresses " + new_list[3] + " or " + new_list[4] + " I am unable to ping ports " + str(new_list[0]) + new_list[1] + " or " + str(new_list[2]) )
---
+++ test_integers/test_integers.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# This is a print message. It prints out the text, "Hello, There"
+
+print("Hello, there!")
+
+# Initialize a variable, a is a simple integar
+a = 2
+
+# This should print out the interger 2
+print(a)
+print("...Check that is printed out 2")
+print() # This prints an empty line
+
+# Initialized anouther cariable, b is a floating-point integer
+
+b = 2.45
+
+# This should print out the floating-point integer
+
+print(b)
+print("...Check that it printed out 2.45")
+
+# Initialize another variable, c is a string
+
+c = "my string"
+
+#This should print out the string 
+
+print(c)
+print("...Check that it printed out 'my string'")
+
+# Here is another wat to print an empty line
+
+print("/n")  # This prints an empty line also
