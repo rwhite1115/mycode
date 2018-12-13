1a111b25e82666aa851df631972fb33d6e5deeb9
rwhite1115 <rwhite1115@gmail.com>
2018-12-13 10:21:28 -0500

    ForLoop2

---
+++ google_size_integers/google-size-integers.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Here are some very large numbers!!!
+num1 = 10**40
+num2 = 10**40 + 1
+
+print("Python supports really large numbers!")
+print()
+
+print("num1 is 10 raised to the 40th power")
+print("num2 is num1, plus1")
+print()
+print("num1 is: ", num1)
+print("num2 is: ", num1)
+
+print()
+print("How precise are these large numbers???")
+print("....Now testing if num1 is less than num2")
+my_bootlean = (num1 < num2)
+print(my_boolean)
+
+print()
+print("....Now testing if num1 is equal to num2")
+my_boolean = (num1 == num2)
+print(my_boolean)
+print()
+
+#######################################
+# A google is a 10 raised to the 100th power
+#######################################
+print("What is a google?????  num3 is a google.  See below.")
+print("num3 is 10 raised to the 100th power")
+print("num4 is num3, plus 1")
+num3 = 10 ** 100
+num4 = 10 ** 100 + 1
